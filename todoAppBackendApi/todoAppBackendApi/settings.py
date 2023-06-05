52e5889747388f71b963b8ac089f3f541f67a467
thisisankit27 <thisisankitsrivastava@gmail.com>
2023-06-05 16:10:14 +0530

    ALLOWED HOSTS

--- todoAppBackendApi/todoAppBackendApi/settings.py
+++ todoAppBackendApi/todoAppBackendApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.173.157.33']
 
 
 # Application definition
