2e8b307b9469afb90c0420e6e7e746fbc5a176fa
thisisankit27 <thisisankitsrivastava@gmail.com>
2023-06-06 15:53:25 +0530

    ALLOWED HOSTS + RDS Instance

--- todoAppBackendApi/todoAppBackendApi/settings.py
+++ todoAppBackendApi/todoAppBackendApi/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.173.157.33']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '52.55.216.140',
+]
 
 
 # Application definition
@@ -91,10 +95,21 @@ WSGI_APPLICATION = 'todoAppBackendApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoRESTApi',
+        'USER': 'todoAppAnkit',
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
     }
 }
 
